9db0afb7a2955c7a8aafa1ebc1485b8af6c2d679
dilipkumarmanavalla <42508793+dilipkumarmanavalla@users.noreply.github.com>
2024-09-02 06:41:17 +0530

    Add DDL queries after testing locally

---
+++ ctva_ddl.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE weather_data (
+    id SERIAL PRIMARY KEY,
+    station_id VARCHAR(50) NOT NULL,
+    date DATE NOT NULL,
+    max_temp INTEGER,
+    min_temp INTEGER,
+    precipitation INTEGER,
+    UNIQUE (station_id, date)
+);
+
+CREATE TABLE weather_stats (
+    id SERIAL PRIMARY KEY,
+    station_id VARCHAR(50) NOT NULL,
+    year INTEGER NOT NULL,
+    avg_max_temp FLOAT,
+    avg_min_temp FLOAT,
+    total_precipitation FLOAT,
+    UNIQUE (station_id, year)
+);
+
